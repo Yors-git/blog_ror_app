21f3a380d8265bcfcab2e759856b8b42666e9948
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-11-12 09:51:06 +0200

    add flash messages for update, destroy and create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
   def edit
@@ -21,11 +22,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
   
+    flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Destroyed!"
     redirect_to action: "index"  
   end
 end
